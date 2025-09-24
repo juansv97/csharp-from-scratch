949eee9098881beb70974c152a1f9908b7bef5bf
Juan Sanchez <juanpas161@gmail.com>
2025-09-23 23:40:37 -0500

    agregada clase cuadrado y rectangulo

---
+++ clases_y_herencia/Program.cs
@@ -0,0 +1,36 @@
+﻿// See https://aka.ms/new-console-template for more information
+// clases
+//var rect = new Rectangulo();
+// rect.Base = 20;
+// rect.Altura = 10;
+// Console.WriteLine($"El area es : {rect.Area()}");
+// Console.WriteLine($"El perimetro es : {rect.Perimetro()}");
+//end clases
+
+var cuadr = new Cuadrado();
+cuadr.Base = 20;
+cuadr.Altura = 40;
+Console.WriteLine($"El area es : {cuadr.Area()}");
+Console.WriteLine($"El perimetro es : {cuadr.Perimetro()}");
+Console.WriteLine($"El lado es : {cuadr.Lado}");
+
+
+class Rectangulo
+{
+    public double Base { get; set; }
+    public double Altura { get; set; }
+
+    public double Area() => Base * Altura;
+
+    public double Perimetro() => 2 * Base + 2 * Altura;
+
+}
+
+class Cuadrado : Rectangulo
+{
+    public double Lado
+    {
+        set { Base = value; Altura = value; }
+        get { return Base; }
+    }
+}
