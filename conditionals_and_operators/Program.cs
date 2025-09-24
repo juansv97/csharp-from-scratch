ea3e77e4121ea9262a9f2371e1240e149ba8eef9
Juan Sanchez <juanpas161@gmail.com>
2025-09-24 14:59:15 -0500

    condicionales y operadores

---
+++ conditionals_and_operators/Program.cs
@@ -0,0 +1,14 @@
+﻿//Condicionales
+//Console.WriteLine("Ingrese un número:");
+//int n = int.Parse(Console.ReadLine());
+//if (n > 0) Console.WriteLine("Positivo");
+//else if (n < 0) Console.WriteLine("Negativo");
+//else Console.WriteLine("Es cero");
+
+//Operadores
+Console.WriteLine("Ingrese el primer número:");
+double a = double.Parse(Console.ReadLine());
+Console.WriteLine("Ingrese el segundo número:");
+double b = double.Parse(Console.ReadLine());
+Console.WriteLine($"Suma: {a+b}, Resta: {a-b}, Multiplicación: {a*b}, División: {(b!=0?a/b:"Indeterminado")}");
+//End Operators
