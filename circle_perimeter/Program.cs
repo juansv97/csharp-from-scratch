0af2d23783cce6f965a0ff3579c2de96ce872ff4
Juan Sanchez <juanpas161@gmail.com>
2025-09-24 19:20:10 -0500

    calcular el perimetro de un circulo

---
+++ circle_perimeter/Program.cs
@@ -0,0 +1,30 @@
+﻿class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("=== Calculadora de Perímetro de un Círculo ===");
+
+        Console.Write("Ingrese el radio del círculo: ");
+        string input = Console.ReadLine();
+
+        if (double.TryParse(input, out double radio) && radio > 0)
+        {
+            var circle = new Circle { Radio = radio };
+            double perimetro = circle.Perimetro();
+            Console.WriteLine($"El perímetro del círculo es: {perimetro:F2}");
+        }
+        else
+        {
+            Console.WriteLine("Entrada inválida. Por favor, ingrese un número positivo.");
+        }
+
+        Console.ReadLine();
+    }
+}
+
+public class Circle
+{
+    public double Radio { get; set; }
+
+    public double Perimetro() => 2 * Math.PI * Radio;
+}
