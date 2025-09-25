cd6abecd631d31512e634701ef016cea6e5a89e7
Juan Sanchez <juanpas161@gmail.com>
2025-09-24 20:35:46 -0500

    muestra el promedio entre cuatro numeros

---
+++ average_of_four/Program.cs
@@ -0,0 +1,23 @@
+﻿class Program
+{
+    static void Main()
+    {
+        System.Console.WriteLine("=== Average of Four ===");
+
+        System.Console.Write("Ingrese el primer número: ");
+        double n1 = double.Parse(System.Console.ReadLine());
+
+        System.Console.Write("Ingrese el segundo número: ");
+        double n2 = double.Parse(System.Console.ReadLine());
+
+        System.Console.Write("Ingrese el tercer número: ");
+        double n3 = double.Parse(System.Console.ReadLine());
+
+        System.Console.Write("Ingrese el cuarto número: ");
+        double n4 = double.Parse(System.Console.ReadLine());
+
+        double promedio = (n1 + n2 + n3 + n4) / 4;
+
+        System.Console.WriteLine($"Resultado: {promedio}");
+    }
+}
