5e22baa7afcb6df6b21f4e97d1cdffa0145a4483
Juan Sanchez <juanpas161@gmail.com>
2025-09-24 20:42:08 -0500

    devuelve el numero de vocales en una palabra

---
+++ vowel_counter/Program.cs
@@ -0,0 +1,22 @@
+﻿class Program
+{
+    static void Main()
+    {
+        System.Console.WriteLine("=== Vowel Counter ===");
+
+        System.Console.Write("Ingrese una palabra: ");
+        string palabra = System.Console.ReadLine().ToLower();
+
+        int contador = 0;
+
+        foreach (char letra in palabra)
+        {
+            if (letra == 'a' || letra == 'e' || letra == 'i' || letra == 'o' || letra == 'u')
+            {
+                contador++;
+            }
+        }
+
+        System.Console.WriteLine($"Resultado: {contador}");
+    }
+}
