7e8fc97a71afed40bd3dadab3dcc2e64d27c5884
Juan Sanchez <juanpas161@gmail.com>
2025-09-24 20:04:51 -0500

    calcula y suma de los numeros entre 1 y 50

---
+++ sum_of_evens/Program.cs
@@ -0,0 +1,26 @@
+﻿class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("=== Sum of Evens ===");
+
+        var calc = new SumOfEvens();
+        int resultado = calc.Calcular();
+
+        Console.WriteLine($"Resultado: {resultado}");
+        Console.ReadLine();
+    }
+}
+
+public class SumOfEvens
+{
+    public int Calcular()
+    {
+        int suma = 0;
+        for (int i = 2; i <= 50; i += 2)
+        {
+            suma += i;
+        }
+        return suma;
+    }
+}
