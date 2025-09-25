5ce20d7cfe8ddc2295a70533f9d5a16b280a883f
Juan Sanchez <juanpas161@gmail.com>
2025-09-24 20:56:41 -0500

    devuelve el factorial de un numero

---
+++ factorial_finder/Program.cs
@@ -0,0 +1,16 @@
+﻿class Program
+{
+    static void Main()
+    {
+        Console.Write("Ingrese un número: ");
+        int numero = int.Parse(Console.ReadLine());
+
+        long factorial = 1;
+        for (int i = 1; i <= numero; i++)
+        {
+            factorial *= i;
+        }
+
+        Console.WriteLine($"El factorial de {numero} es: {factorial}");
+    }
+}
