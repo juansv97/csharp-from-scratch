6f90a3623c3f6ef192602db254c4714b165019eb
Juan Sanchez <juanpas161@gmail.com>
2025-09-24 20:31:54 -0500

    muestra la longitud de una palabra

---
+++ string_lenght/Program.cs
@@ -0,0 +1,13 @@
+﻿class Program
+{
+    static void Main()
+    {
+        System.Console.WriteLine("=== String Length ===");
+        System.Console.Write("Ingrese una palabra: ");
+        string palabra = System.Console.ReadLine();
+
+        int longitud = palabra.Length;
+
+        System.Console.WriteLine($"Resultado: {longitud}");
+    }
+}
